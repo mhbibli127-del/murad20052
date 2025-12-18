3ff2e72910aa903a64750f96468beb1bb77e226d
mhbibli127-del <mhbibli127@gmail.com>
2025-12-19 01:00:00 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const Username = "My name is murad";
+console.log(Username);
+
+
+let UserName = "2user";
+let card = "c@rd";
+let person = "person";
+let titleColor = "title color";
+console.log(UserName);
+console.log(card);
+console.log(person);
+console.log(titleColor);
+console.log("$add");
+
+
+let bookPrice = 750;
+let bookAmount = 14;
+
+console.log (bookPrice + bookAmount);
+
+let kmhSpeed = 120;
+let msSpeed = 5;
+
+console.log (kmhSpeed / msSpeed);
+
+
+let birthdayinput = prompt ("Enter your birthday");
+
+let ageinput = prompt ("Enter your age");
+
+
+let daysinput = prompt ("Enter days in your life");
+
+console.log(`My birthday is ${birthdayinput}, I am ${ageinput} years old, and I have lived for ${daysinput} days.`);
+
+let num = 5**3/3;
+console.log(num);
+
+
+let firstName = "Alan";
+let lastName = "Turing";
+console.log( `Tanışlıgımıza çox şadam. ${firstName},${lastName}`);
+
+let Userinput = prompt ("Input a number");
+let Userinput2 = prompt ("Input a second number");
+
+console.log (Userinput * Userinput2, Userinput / Userinput2, Userinput - Userinput2, Userinput + Userinput2);
+
+const Userage = 16;
+
+if(Userage < 16) {
+    console.log(prompt ( "Your are so younger for acces to this page"));
+}
+else (Userage>=16); {
+console.log(prompt ("Welcome to he page"));
+}
